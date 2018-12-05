741ccd6cacc9f3b4e8cc6c8ff324b875bcb23efa
张和 <zhanghe0@126.com>
2018-12-05 17:58:42 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import Koa from 'koa'
+import json from 'koa-json'
+import bodyParser from 'koa-bodyparser'
+import logger from 'koa-logger'
+import session from 'koa-session'
+import compress from 'koa-compress'
+import convert from 'koa-convert'
+
+const app = new Koa()
+console.log(app)
+//设置签名的 Cookie 密钥
+app.keys = ['this is a fucking secret']
+
+app.use(convert(session(app)))
+app.use(compress())
+app.use(bodyParser())
+app.use(json())
+app.use(logger())
+
+export default app
